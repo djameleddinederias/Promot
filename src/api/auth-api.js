83e17ffb56ea5b2630f83833169c708006335d97
akram09 <a.boutouchent@esi-sba.dz>
2020-02-10 21:04:29 +0100

    add the template

---
+++ src/api/auth-api.js
@@ -0,0 +1,126 @@
+import firebase from "react-native-firebase";
+import "firebase/auth";
+import { AccessToken, LoginManager } from 'react-native-fbsdk';
+import firebaseRn from 'react-native-firebase'
+
+export const logoutUser = () => {
+  firebase.auth().signOut();
+};
+
+export const loginWithFacebook = async ()=>{
+  try {
+    const result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
+
+    if (result.isCancelled) {
+      // handle this however suites the flow of your app
+      throw new Error('User cancelled request'); 
+    }
+
+    console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`);
+
+    // get the access token
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      // handle this however suites the flow of your app
+      throw new Error('Something went wrong obtaining the users access token');
+    }
+
+    // create a new firebase credential with the token
+    const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+
+    // login with credential
+    const firebaseUserCredential = await firebaseRn.auth().signInWithCredential(credential);
+
+    console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()))
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const signInUser = async ({ name, email, password }) => {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    firebase.auth().currentUser.updateProfile({
+      displayName: name
+    });
+
+    return {};
+  } catch (error) {
+    switch (error.code) {
+      case "auth/email-already-in-use":
+        return {
+          error: "E-mail already in use."
+        };
+      case "auth/invalid-email":
+        return {
+          error: "Invalid e-mail address format."
+        };
+      case "auth/weak-password":
+        return {
+          error: "Password is too weak."
+        };
+      case "auth/too-many-requests":
+        return {
+          error: "Too many request. Try again in a minute."
+        };
+      default:
+        return {
+          error: "Check your internet connection."
+        };
+    }
+  }
+};
+
+export const loginUser = async ({ email, password }) => {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+    return {};
+  } catch (error) {
+    switch (error.code) {
+      case "auth/invalid-email":
+        return {
+          error: "Invalid email address format."
+        };
+      case "auth/user-not-found":
+      case "auth/wrong-password":
+        return {
+          error: "Invalid email address or password."
+        };
+      case "auth/too-many-requests":
+        return {
+          error: "Too many request. Try again in a minute."
+        };
+      default:
+        return {
+          error: "Check your internet connection."
+        };
+    }
+  }
+};
+
+export const sendEmailWithPassword = async email => {
+  try {
+    await firebase.auth().sendPasswordResetEmail(email);
+    return {};
+  } catch (error) {
+    switch (error.code) {
+      case "auth/invalid-email":
+        return {
+          error: "Invalid email address format."
+        };
+      case "auth/user-not-found":
+        return {
+          error: "User with this email does not exist."
+        };
+      case "auth/too-many-requests":
+        return {
+          error: "Too many request. Try again in a minute."
+        };
+      default:
+        return {
+          error: "Check your internet connection."
+        };
+    }
+  }
+};
---
+++ src/core/config.js
@@ -0,0 +1,11 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCcoCI1pLgjCIQQFMYz-AWWSK7KL8iqhfo",
+  authDomain: "promot-54aaf.firebaseapp.com",
+  databaseURL: "https://promot-54aaf.firebaseio.com",
+  projectId: "promot-54aaf",
+  storageBucket: "promot-54aaf.appspot.com",
+  messagingSenderId: "1058684385835",
+  appId: "1:1058684385835:web:e9a4e3f0d4e774a6b8086e",
+  measurementId: "G-7RM9QPQH71"
+};
